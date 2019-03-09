c0b62af4b7d400624911fc0f7a476ee15f812554
Vineet Reynolds Pereira <Vineet.Reynolds@gmail.com>
2019-03-10 05:26:46 +0530

    fixed parameterized tests.

--- tests/test_nonrepeatingelements.py
+++ tests/test_nonrepeatingelements.py
@@ -8,27 +8,27 @@ class NonRepeatingElementsTest(unittest.TestCase):
     ("Array single element non repeatable", [1], {1}),
     ("Array multiple unique element multiple non repeatable", [1,2,3], {1,2,3})
   ])
-  def test_find_unique_nonrepeating_elements(self, name, input, expected):
+  def test_find_unique_nonrepeating_elements(self, name, input_value, expected):
     self.longMessage = True
-    actual = findnonrepeatingelements(input)
-    message = 'For input {0}, expected value = {1}, and actual value = {2}'.format(input, expected, actual)
+    actual = findnonrepeatingelements(input_value)
+    message = 'For input {0}, expected value = {1}, and actual value = {2}'.format(input_value, expected, actual)
     self.assertEqual(expected, actual, message)
 
   @parameterized.expand([
     ("Array multiple element single non repeatable", [1,2,3,2,1], {3}),
     ("Array multiple element multiple non repeatable", [1, 2, 2, 3, 1, 4, 1, 3, 67], {4, 67})
   ])
-  def test_find_nonrepeating_elements_among_duplicates(self, name, input, expected):
+  def test_find_nonrepeating_elements_among_duplicates(self, name, input_value, expected):
     self.longMessage = True
-    actual = findnonrepeatingelements(input)
-    message = 'For input {0}, expected value = {1}, and actual value = {2}'.format(input, expected, actual)
+    actual = findnonrepeatingelements(input_value)
+    message = 'For input {0}, expected value = {1}, and actual value = {2}'.format(input_value, expected, actual)
     self.assertEqual(expected, actual, message)
 
   @parameterized.expand([
     ("Array multiple element single repeatable", [1,1], set())
   ])
-  def test_find_duplicate_elements(self, name, input, expected):
+  def test_find_duplicate_elements(self, name, input_value, expected):
     self.longMessage = True
-    actual = findnonrepeatingelements(input)
-    message = 'For input {0}, expected value = {1}, and actual value = {2}'.format(input, expected, actual)
+    actual = findnonrepeatingelements(input_value)
+    message = 'For input {0}, expected value = {1}, and actual value = {2}'.format(input_value, expected, actual)
     self.assertEqual(expected, actual, message)
