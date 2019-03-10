cc3282ce7e94e9887104ab87e1f26617d3354d08
Vineet Reynolds Pereira <Vineet.Reynolds@gmail.com>
2019-03-10 08:47:57 +0530

    Added instructions and main method.

--- nonrepeatingelements.py
+++ nonrepeatingelements.py
@@ -1,3 +1,18 @@
 
 def findnonrepeatingelements(elements):
-  return {None}
+  #
+  # IMPORTANT:
+  # 1. Write your solution in this function
+  # 2. Do not make changes to the function signature
+  # 3. Use the 'pip-install' command in the IDE, to install any requirements. This takes a few seconds.
+  # 4. Use the 'run' command in the IDE, to run the main method. You can also invoke the main method from the terminal.
+  # 5. Use the 'run-tests' command in the IDE, to run unit tests. The 'pip-install' command must be run before this.
+  return None
+
+def main():
+  sample_list = [1, 2, 3]
+  non_repeating_elements = findnonrepeatingelements(sample_list)
+  print('Non repeating elements in list {0} are: {1}\n'.format(sample_list, non_repeating_elements))
+
+if __name__ == '__main__':
+  main()
